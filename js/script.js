24c863c8c1263643c27a5cb3003e1fb88401fff3
billwang7599 <billwang7599@gmail.com>
2021-12-26 03:41:09 -0500

    added downscroll buttons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 $("#resume-button").click(function () {
     window.open("media/resume.pdf", "_blank");
 });
+
+$("#to-intro").click(function () {
+    $("html, body").animate({
+        scrollTop: $("#intro").offset().top
+    }, 500)
+});
+
+$("#to-profs").click(function () {
+    $("html, body").animate({
+        scrollTop: $("#profs").offset().top
+    }, 500)
+});
+
+$("#to-contacts").click(function () {
+    $("html, body").animate({
+        scrollTop: $("#contacts").offset().top
+    }, 500)
+});
